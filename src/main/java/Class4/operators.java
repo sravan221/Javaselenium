3482845f942910f5554a9b43fddbc84b8afb7523
sravan221 <sraone333@gmail.com>
2022-11-10 14:59:47 +0000

    added all the files from java training

---
+++ src/main/java/Class4/operators.java
@@ -0,0 +1,21 @@
+package Class4;
+
+public class operators {
+
+    public static void main(String[] args) {
+
+        int x=20;
+        int y=10;
+
+        int sum= x+y;
+        System.out.println("sum:"+sum);
+        int sub=x-y;
+        System.out.println("sub:"+sub);
+        int div=x/y;
+        System.out.println("div:"+div);
+        int mul=x*y;
+        System.out.println("mul:"+mul);
+        int rem=x%y;
+        System.out.println("rem:"+rem);
+    }
+}
